7be1cebedc7b138d0094d5904837a1f2fa832cc7
htao <htao@thoughtworks.com>
2018-04-08 15:38:37 +0800

    [taohui] print notify message unless user input 3

--- src/main/java/GeneralApplication.java
+++ src/main/java/GeneralApplication.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class GeneralApplication {
     public static void main(String[] args){
         GeneralApplication app = new GeneralApplication();
         System.out.println(app.printHelpMessage());
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+
+        while(true){
+            if(s.chars().allMatch(Character::isDigit) && Integer.parseInt(s) == 3) {
+                System.out.println("thank you");
+                break;
+            }
+            System.out.println("your input is: " + s);
+            System.out.println(app.printHelpMessage());
+            s = scanner.nextLine();
+        }
+
     }
 
     public String printHelpMessage(){
