c5095630c9c107e1d02123bed51831b51e1ec448
htao <htao@thoughtworks.com>
2018-04-08 15:04:38 +0800

    [taohui] implement first test structure

---
+++ src/main/java/GeneralApplication.java
@@ -0,0 +1,10 @@
+public class GeneralApplication {
+    public static void main(String[] args){
+        GeneralApplication app = new GeneralApplication();
+        System.out.println(app.print());
+    }
+
+    public String print(){
+        return "hello";
+    }
+}
---
+++ src/test/java/GeneralApplicationTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class GeneralApplicationTest {
+    @Test
+    public void myFirstTest(){
+        assertEquals(2, 1+1);
+    }
+}
